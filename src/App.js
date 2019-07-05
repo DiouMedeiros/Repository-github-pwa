80d613a04f10ec7e43a0371a539f3b650d390fdb
DiouMedeiros <diou.182@gmail.com>
2019-07-05 20:10:21 -0300

    Implementao controle offline e cache

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React, { Component, Fragment } from 'react';
-import axios from 'axios';
+import React, { Component, Fragment } from "react";
+import axios from "axios";
 
-import { Header, Repositories } from './styles';
+import { Header, Repositories, Offline } from "./styles";
 
 export default class App extends Component {
   state = {
-    newRepoInput: '',
-    repositories: []
+    online: navigator.onLine,
+    newRepoInput: "",
+    repositories:
+      JSON.parse(localStorage.getItem("@diouMedeiros:repositories")) || []
+  };
+
+  componentDidMount() {
+    window.addEventListener("online", this.handleNetworkChange);
+    window.addEventListener("offline", this.handleNetworkChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("online", this.handleNetworkChange);
+    window.removeEventListener("offline", this.handleNetworkChange);
+  }
+
+  handleNetworkChange = () => {
+    this.setState({ online: navigator.online });
   };
 
   addRepository = async () => {
     if (!this.state.newRepoInput) return;
 
-    const response = await axios.get(`https://api.github.com/repos/${this.state.newRepoInput}`);
+    if (!this.state.online) {
+      alert("Você está offline, conecte-se para fazer essa ação!");
+    }
+
+    const response = await axios.get(
+      `https://api.github.com/repos/${this.state.newRepoInput}`
+    );
 
     this.setState({
-      newRepoInput: '',
-      repositories: [ ... this.state.repositories, response.data]
-    })
-  }
+      newRepoInput: "",
+      repositories: [...this.state.repositories, response.data]
+    });
+
+    localStorage.setItem(
+      "repositories",
+      JSON.stringify(this.state.repositories)
+    );
+  };
 
   render() {
     return (
@@ -30,13 +57,11 @@ export default class App extends Component {
             value={this.state.newRepoInput}
           />
 
-          <button onClick={this.addRepository}>
-            Adicionar
-          </button>
+          <button onClick={this.addRepository}>Adicionar</button>
         </Header>
 
         <Repositories>
-          { this.state.repositories.map( repository => (
+          {this.state.repositories.map(repository => (
             <li key={repository.id}>
               <img src={repository.owner.avatar_url} />
               <div>
@@ -45,8 +70,10 @@ export default class App extends Component {
                 <a href={repository.html_url}>Acessar</a>
               </div>
             </li>
-          )) }
+          ))}
         </Repositories>
+
+        {!this.state.online && <Offline>Você está Offline</Offline>}
       </Fragment>
     );
   }
--- src/styles.js
+++ src/styles.js
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     * {
@@ -17,61 +17,109 @@ export const GlobalStyle = createGlobalStyle`
 `;
 
 export const Header = styled.header`
-    width: 100%;
-    background: #7159c1;
-    font-size: 18px;
-    color: #FFF;
+  width: 100%;
+  background: #7159c1;
+  font-size: 18px;
+  color: #fff;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+  padding: 10px 20px;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  button {
+    background: transparent;
+    border: 0;
     font-weight: 600;
-    display: flex;
-    align-items: center;
-    padding: 10px 20px;
+    color: #fff;
+    font-size: 16px;
+    cursor: pointer;
+  }
 
-    position: fixed;
-    top: 0;
-    left: 0;
-
-    button {
-        background: transparent;
-        border: 0;
-        font-weight: 600;
-        color: #fff;
-        font-size: 16px;
-        cursor: pointer;
-    }
-
-    input {
-        flex: 1;
-        border: 0;
-        background: #fff;
-        color: #333;
-        height: 32px;
-        font-size: 14px;
-        border-radius: 4px;
-        padding: 0 10px;
-        margin-left: 10px;
-    }
+  input {
+    flex: 1;
+    border: 0;
+    background: #fff;
+    color: #333;
+    height: 32px;
+    font-size: 14px;
+    border-radius: 4px;
+    padding: 0 10px;
+    margin-left: 10px;
+  }
 `;
 
 export const Repositories = styled.ul`
-    list-style: none;
-    margin: 70px 20px 20px;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-column-gap: 20px;
-    grid-row-gap: 20px;
+  list-style: none;
+  margin: 70px 20px 20px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
 
-    li {
-        flex: 1;
-        padding: 20px;
-        display: flex;
-        background: #fff;
-        box-shadow: 0 0 10px 0 rgba(221, 221, 221, 0.5);
-        border-radius: 4px;
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 
-        img {
-            height: 60px;
-            width: 60px;
-            border-radius: 50%;
-        }
+  @media (max-width: 600px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
+  li {
+    flex: 1;
+    padding: 20px;
+    display: flex;
+    background: #fff;
+    box-shadow: 0 0 10px 0 rgba(221, 221, 221, 0.5);
+    border-radius: 4px;
+
+    img {
+      height: 60px;
+      width: 60px;
+      border-radius: 50%;
     }
+
+    div {
+      flex: 1;
+      margin-left: 15px;
+
+      strong {
+        color: #333;
+        font-size: 16px;
+      }
+
+      p {
+        font-size: 14px;
+        color: #666;
+        margin-top: 5px;
+      }
+
+      a {
+        color: #7159c1;
+        font-size: 14px;
+        font-weight: bold;
+        margin-top: 10px;
+        text-decoration: none;
+        text-transform: uppercase;
+        display: block;
+      }
+    }
+  }
+`;
+
+export const Offline = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  height: 40px;
+  width: 100%;
+  background: #c15959;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 600;
 `;
